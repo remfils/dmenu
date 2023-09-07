269158615c2b739f67b35f71cfa63057a661ede5
remfils <remfils@tresio-deb.zuzino.net>
2023-09-07 13:17:10 -0400

    theme

--- config.def.h
+++ config.def.h
@@ -9,9 +9,9 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#4c4f69", "#eff1f5" },
+	[SchemeSel] = { "#4c4f69", "#04a5e5" },
+	[SchemeOut] = { "#000000", "#04a5e5" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
