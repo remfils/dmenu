7da4ee750a76203900f97619f5c42f1fc49439f3
remfils <remfils@tresio-deb.zuzino.net>
2023-09-09 21:58:27 +0300

    [patch] center

--- config.def.h
+++ config.def.h
@@ -2,16 +2,21 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 1;                    /* -c option; centers dmenu on screen */
+static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"monospace:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+
+static const char col_dark_green[] = "#287e40";
+
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#4c4f69", "#eff1f5" },
-	[SchemeSel] = { "#4c4f69", "#04a5e5" },
-	[SchemeOut] = { "#000000", "#04a5e5" },
+	[SchemeSel] = { "#4c4f69", col_dark_green },
+	[SchemeOut] = { "#000000", col_dark_green },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
